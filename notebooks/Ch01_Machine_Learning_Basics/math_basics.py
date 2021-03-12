2e7727e44d8283704bcede8d916e80e6c22e5c90
Lakkireddy Rama Narayana Reddy <lakkireddyrnreddy@gmail.com>
2021-03-12 00:04:26 -0600

    Update math_basics.py

--- notebooks/Ch01_Machine_Learning_Basics/math_basics.py
+++ notebooks/Ch01_Machine_Learning_Basics/math_basics.py
@@ -43,11 +43,6 @@ print ('Matrix Determinant:', np.linalg.det(m), '\n')
 m_inv = np.linalg.inv(m)
 print ('Matrix inverse:\n', m_inv, '\n')
 
-# identity matrix (result of matrix x matrix_inverse)
-iden_m =  np.dot(m, m_inv)
-iden_m = np.round(np.abs(iden_m), 0)
-print ('Product of matrix and its inverse:\n', iden_m)
-
 
 # eigendecomposition
 m = np.array([[1, 5, 2],
