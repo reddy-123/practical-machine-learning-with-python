d75aa848455d41d78b0249f88aa8175daa1e1eac
Lakkireddy Rama Narayana Reddy <lakkireddyrnreddy@gmail.com>
2021-03-12 00:04:58 -0600

    Update nlp_example.py

--- notebooks/Ch01_Machine_Learning_Basics/nlp_example.py
+++ notebooks/Ch01_Machine_Learning_Basics/nlp_example.py
@@ -9,9 +9,6 @@ from nltk.parse.stanford import StanfordParser
 
 sentence = 'The quick brown fox jumps over the lazy dog'
 
-# create parser object
-scp = StanfordParser(path_to_jar='E:/stanford/stanford-parser-full-2015-04-20/stanford-parser.jar',
-                   path_to_models_jar='E:/stanford/stanford-parser-full-2015-04-20/stanford-parser-3.5.2-models.jar')
 
 
 # get parse tree
