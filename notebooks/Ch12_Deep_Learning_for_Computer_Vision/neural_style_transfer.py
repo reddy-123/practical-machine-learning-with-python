1e6815833aeb68b2e9c9af1d680a2ea1f45e72a8
Lakkireddy Rama Narayana Reddy <lakkireddyrnreddy@gmail.com>
2021-03-16 01:48:44 -0500

    Update neural_style_transfer.py

--- notebooks/Ch12_Deep_Learning_for_Computer_Vision/neural_style_transfer.py
+++ notebooks/Ch12_Deep_Learning_for_Computer_Vision/neural_style_transfer.py
@@ -83,7 +83,22 @@ class Evaluator(object):
         self.height = height
         self.width = width
 
-    
+    def loss(self, x):
+        assert self.loss_value is None
+        x = x.reshape((1, self.height, self.width, 3))
+        outs = fetch_loss_and_grads([x])
+        loss_value = outs[0]
+        grad_values = outs[1].flatten().astype('float64')
+        self.loss_value = loss_value
+        self.grad_values = grad_values
+        return self.loss_value
+
+    def grads(self, x):
+        assert self.loss_value is not None
+        grad_values = np.copy(self.grad_values)
+        self.loss_value = None
+        self.grad_values = None
+        return grad_values
     
 
 
